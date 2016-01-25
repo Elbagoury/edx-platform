1b116b3d8cd026c6df5e0406b1ec7d927a72367d
vkaracic <karacicvedran@gmail.com>
2016-01-25 15:49:23 +0000

    Check to see if it's a microsite

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -884,14 +884,17 @@ def course_about(request, course_id):
                 reg_url=reverse('register_user'), course_id=urllib.quote(str(course_id)))
 
         ecommerce_checkout_link = ''
-        if config.checkout_on_ecommerce_service and (
+        if config.checkout_on_ecommerce_service and not microsite.is_request_in_microsite() and (
             'professional' in modes or 'no-id-professional' in modes
         ):
             mode = modes.get('professional', '') or modes.get('no-id-professional', '')
+
             ecom_url = urljoin(settings.ECOMMERCE_PUBLIC_URL_ROOT, config.single_course_checkout_page)
             ecommerce_checkout_link = "%s?sku=%s" % (ecom_url, mode.sku)
+
             reg_then_add_to_cart_link = "{reg_url}?course_id={course_id}&enrollment_action=add_to_ecomm_cart&checkout_url={checkout_url}".format(
                 reg_url=reverse('register_user'), course_id=urllib.quote(str(course_id)), checkout_url=ecommerce_checkout_link)
+
         course_price = get_cosmetic_display_price(course, registration_price)
         can_add_course_to_cart = _is_shopping_cart_enabled and registration_price
 
